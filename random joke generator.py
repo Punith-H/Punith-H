20cc2be68d73c497f62e0610d764099d03fe94e7
Punith-H <hpunith68@gmail.com>
2024-12-19 09:43:02 -0800

    Create random joke generator.py

---
+++ random joke generator.py
@@ -0,0 +1,10 @@
+import random
+
+jokes = [
+    "Why don't skeletons fight each other? They don't have the guts.",
+    "Why did the scarecrow win an award? Because he was outstanding in his field.",
+    "Why don't scientists trust atoms? Because they make up everything!"
+]
+print("Random Joke Generator")
+print("Here's a joke for you:")
+print(random.choice(jokes))
